267fa569569f8cfdeecee4c38b8ee0693468b616
Andrew Brice <andrewbrice@Andrews-MBP.lan>
2023-09-22 11:25:21 -0400

    refactor: implement react-jss and basic theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import { Logo } from "./components";
 
-import "./App.css";
+import { createUseStyles, ThemeProvider } from "react-jss";
 
-function App() {
+const useStyles = createUseStyles((theme) => ({
+  root: {
+    alignItems: 'center',
+    background: theme.background.primary,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    minHeight: '100vh',
+    width: '100vw',
+  },
+}));
+
+const theme = {
+  background: {
+    primary: "#F3EDE4"
+  },
+  text: {
+    secondary: '#8D866E'
+  }
+};
+
+const Main = (props) => {
+  const classes = useStyles();
+
+  return <div className={classes.root} {...props} />;
+};
+
+const App = () => {
   return (
-    <section>
-      <Logo alt="Andrew Brice logo" />
-      <h1>Andrew Brice</h1>
-      <p>Software Engineer</p>
-    </section>
+    <ThemeProvider theme={theme}>
+      <Main>
+        <Logo />
+      </Main>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
