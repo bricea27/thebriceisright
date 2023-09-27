a83270771d15f14e45981dceb23114fc111c29f7
Andrew Brice <bricea27@gmail.com>
2023-09-26 20:02:17 -0400

    feat: update headshot, spread default colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Logo from "./components/Logo";
 
-import headshot from "./assets/headshot.jpg";
+import headshot from "./assets/headshot.png";
 
 export default function App() {
   return (
     <main className="bg-background flex h-screen overflow-hidden w-screen">
       <div className="w-1/2 h-screen p-4">
         <Logo className="max-w-[100px] stroke-text-secondary" />
-      </div>
-      <div className="w-1/2">
-        <img alt="Andrew Brice headshot" src={headshot} />
+        <img
+          className="absolute right-0 top-12 w-1/2 max-w-3xl"
+          alt="Andrew Brice headshot"
+          src={headshot}
+        />
       </div>
     </main>
   );
