08309fb9f2ad505bd47824f9c53a976712d544c5
Andrew Brice <bricea27@gmail.com>
2023-09-27 03:49:12 -0400

    feat: address responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ import headshot from "./assets/headshot.png";
 export default function App() {
   return (
     <main className="bg-background flex h-screen overflow-hidden w-screen">
-      <div className="w-1/2 h-screen p-8">
-        <Logo className="max-w-[100px] mb-20 stroke-text-secondary" />
-        <h1 className="font-sans font-extrabold mb-4 text-6xl text-text-primary">
+      <div className="md:w-1/2 sm:w-full p-8 z-10">
+        <Logo className="max-w-[80px] md:max-w-[100px] mb-20 stroke-text-secondary" />
+        <h1 className="font-sans font-extrabold mb-4 text-6xl text-text-primary tracking-tight drop-shadow-sm">
           Hello,
           <br />
           I'm Andrew Brice
         </h1>
-        <p className="font-sans font-light text-text-secondary text-2xl">
+        <p className="font-sans font-light text-text-primary text-2xl">
           With nearly a decade of experience as a software engineer and mentor,
           I leverage my background in design to build clean, user-friendly, and
           dynamic web interfaces.
         </p>
       </div>
       <img
-        className="absolute right-0 top-12 w-1/2 max-w-3xl"
+        className="absolute -bottom-10 right-0 opacity-20 md:opacity-100 max-w-md md:max-w-l lg:max-w-xl"
         alt="Andrew Brice headshot"
         src={headshot}
       />
