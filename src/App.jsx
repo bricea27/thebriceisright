4fbe95caee2605504bd07b1fdaeab1e5731e7f59
Andrew Brice <bricea27@gmail.com>
2023-09-30 00:54:47 -0400

    feat: implement basic quote fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import React from "react";
+
 import styles from "./App.module.css";
 
 import Logo from "./components/Logo";
 
 export default function App() {
+  const [reference, setReference] = React.useState();
+
+  const fetchQuote = async () => {
+    const response = await fetch(
+      "https://api.chucknorris.io/jokes/random?category=dev&name=Andrew"
+    );
+    const { value } = await response.json();
+
+    const user = await fetch("https://randomuser.me/api/");
+    const { results } = await user.json();
+
+    setReference({
+      ...results[0],
+      value,
+    });
+  };
+
+  React.useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
-    <main className="w-full min-h-screen bg-background p-8 lg:p-16 font-sans text-text-primary bg-gradient-to-br from-background to-background-light flex flex-col items-center">
+    <main className="w-full min-h-screen bg-background p-8 lg:p-16 font-sans text-text-primary flex flex-col items-center">
       <header className="animate animate-fade-in-up w-full max-w-lg lg:max-w-2xl mb-8 lg:mb-16">
         <Logo
           id={styles.logo}
@@ -14,7 +37,7 @@ export default function App() {
           Andrew Brice
         </h1>
         <h2 className="font-sans font-light text-lg lg:text-2xl">
-          Software Engineer
+          UI Engineer
         </h2>
       </header>
       <article className="w-full max-w-lg lg:max-w-2xl mb-8 lg:mb-16">
@@ -25,7 +48,7 @@ export default function App() {
           user-friendly web applications.
         </p>
         <p className="font-light text-base lg:text-xl mb-4 lg:mb-6">
-          Born in New York City and raised just across the Hudson river in New
+          Born in New York City and raised across the Hudson river in New
           Jersey, Andrew took a job in Indianapolis in 2015 and has since called
           it his home. He lives in Indy with his wife, son, and two dogs.
         </p>
@@ -37,6 +60,49 @@ export default function App() {
           buttered popcorn (layered, of course).
         </p>
       </article>
+      {reference?.value && (
+        <article className="w-full max-w-lg lg:max-w-2xl mb-8 lg:mb-16">
+          <h3 className="font-bold text-lg lg:text-2xl mb-4 lg:mb-6">
+            References
+          </h3>
+          <div className="px-4 py-8 mx-auto text-center bg-text-secondary bg-opacity-10 rounded-2xl relative">
+            <figure className="max-w-screen-md mx-auto">
+              <svg
+                className="h-12 mx-auto mb-3 text-text-secondary opacity-30"
+                viewBox="0 0 24 27"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                  fill="currentColor"
+                />
+              </svg>
+              <blockquote>
+                <p className="font-medium  text-base lg:text-xl">
+                  "{reference.value}"
+                </p>
+              </blockquote>
+              <figcaption className="flex items-center justify-center mt-6 space-x-3">
+                <img
+                  className="w-6 h-6 rounded-full"
+                  src={reference.picture.thumbnail}
+                  alt="profile picture"
+                />
+                <div className="font-normal text- dark:text-white">
+                  {reference?.name.first} {reference.name.last}
+                </div>
+              </figcaption>
+            </figure>
+            <button
+              className="bg-text-secondary bg-opacity-80 transition-opacity hover:bg-opacity-100 text-background font-bold py-2 px-4 rounded-2xl mx-auto absolute top-4 right-4"
+              onClick={fetchQuote}
+            >
+              Next
+            </button>
+          </div>
+        </article>
+      )}
     </main>
   );
 }
