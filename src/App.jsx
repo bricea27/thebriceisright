4735f8b8756e90f0de2850d3adffa422247635c7
Andrew Brice <bricea27@gmail.com>
2023-10-03 20:32:03 -0400

    new layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import styles from "./App.module.css";
 import Link from "./components/Link";
 import Logo from "./components/Logo";
 
-import headshot from "./assets/headshot.png";
+import headshot from "./assets/headshot.jpg";
 
 export default function App() {
   const [quote, setQuote] = useState(null);
@@ -52,49 +52,10 @@ export default function App() {
       </section>
 
       <header className="w-full max-w-lg lg:max-w-2xl flex items-center justify-between mx-auto p-8 sm:px-0 sm:py-12">
-        <Logo
-          id={styles.logo}
-          className="w-[60px] lg:w-[80px] stroke-text-secondary stroke-[10px]"
-        />
-        <div className="flex items-center gap-4 sm:gap-8">
-          <Link href="https://github.com/bricea27">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"
-              />
-            </svg>
-            <span>Code</span>
-          </Link>
-          <Link href="https://www.linkedin.com/in/briceandrew/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
-              />
-            </svg>
-            Work history
-          </Link>
-        </div>
+        <Logo className="w-[60px] lg:w-[80px] stroke-text-secondary stroke-[10px]" />
       </header>
       <main className="w-full flex flex-col items-center">
-        <section className="w-full h-72 lg:h-96 max-w-2xl lg:max-w-4xl flex place-content-center mb-8 lg:mb-16 bg-text-secondary bg-opacity-10 sm:rounded-3xl overflow-hidden">
+        {/* <section className="w-full h-72 lg:h-96 max-w-2xl lg:max-w-4xl flex place-content-center mb-8 lg:mb-16 bg-text-secondary bg-opacity-10 sm:rounded-3xl overflow-hidden">
           <article className="w-full max-w-lg lg:max-w-2xl rounded-3xl flex items-center justify-between pl-8 sm:pl-0 relative">
             <div>
               <h1 className="font-sans font-extrabold text-2xl xs:text-3xl lg:text-5xl leading-none">
@@ -109,7 +70,7 @@ export default function App() {
               src={headshot}
             />
           </article>
-        </section>
+        </section> */}
         <article className="w-full max-w-lg lg:max-w-2xl mb-4 lg:mb-8 px-8 sm:p-0">
           <h3 className="font-bold text-lg lg:text-2xl mb-4 lg:mb-6">About</h3>
           <p className="font-light text-base lg:text-xl mb-4 lg:mb-6">
@@ -130,7 +91,7 @@ export default function App() {
           </p>
         </article>
 
-        <section className="w-screen flex justify-center mb-8 lg:mb-16 py-4 sm:py-8 bg-white bg-opacity-50">
+        <section className="w-full max-w-2xl lg:max-w-3xl flex place-content-center mb-8 lg:mb-16 bg-text-secondary bg-opacity-10 sm:rounded-3xl overflow-hidden">
           <article className="w-full max-w-lg lg:max-w-2xl px-8 sm:p-0">
             {/* 
             <h3 className="font-bold text-lg lg:text-2xl mb-4 lg:mb-6">
@@ -208,33 +169,77 @@ export default function App() {
           </article>
         </section>
 
-        <article className="w-full max-w-lg lg:max-w-2xl mb-8 lg:mb-16 px-8 sm:p-0">
-          <h3 className="font-bold text-lg lg:text-2xl mb-4 lg:mb-6">
-            Contact
-          </h3>
-          <p className="font-light text-base lg:text-xl mb-4 lg:mb-6">
-            Whether you'd like to discuss an opportunity or simply talk shop,
-            <br />
-            I'd love to get in touch!
-          </p>
-          <Link href="mailto:bricea27@gmail.com">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-              />
-            </svg>
-            bricea27@gmail.com
-          </Link>
-        </article>
+        <section className="w-screen bg-text-primary">
+          <article className="w-full max-w-lg lg:max-w-2xl py-32 mx-auto">
+            <div className="w-full flex items-center justify-between gap-8">
+              <div className="flex flex-col">
+                <h3 className="font-bold text-lg lg:text-2xl mb-4 lg:mb-6 text-background">
+                  Get in touch
+                </h3>
+                <p className="font-light text-base lg:text-xl mb-4 lg:mb-6 text-background">
+                  Whether you'd like to discuss an opportunity or simply talk
+                  shop,
+                  <br />
+                  I'd love to connect!
+                </p>
+                <Link href="mailto:bricea27@gmail.com">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 mb-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                    />
+                  </svg>
+                  bricea27@gmail.com
+                </Link>
+                <Link href="https://github.com/bricea27">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 mb-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"
+                    />
+                  </svg>
+                  <span>Github</span>
+                </Link>
+                <Link href="https://www.linkedin.com/in/briceandrew/">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
+                    />
+                  </svg>
+                  LinkedIn
+                </Link>
+              </div>
+              <div className="w-2/3 overflow-hidden rounded-3xl bg-red-300">
+                <img className="" src={headshot} />
+              </div>
+            </div>
+          </article>
+        </section>
       </main>
     </div>
   );
