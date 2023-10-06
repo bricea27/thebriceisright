27d5642a16a2745c97fee8b788578d01577421d7
Andrew Brice <bricea27@gmail.com>
2023-10-06 00:45:56 -0400

    fix: remove unused code

--- src/components/Article/ArticleHeading.jsx
+++
@@ -1,5 +0,0 @@
-export default function ArticleHeading(props) {
-  return (
-    <h3 className="font-bold text-base lg:text-xl mb-2 lg:mb-4" {...props} />
-  );
-}
--- src/components/Article/index.js
+++
@@ -1,3 +0,0 @@
-export { default } from './Article';
-export { default as ArticleBody } from './ArticleBody'
-export { default as ArticleHeading } from './ArticleHeading'
