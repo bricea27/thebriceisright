1bafc69420ec56479a94c1e954973157b85e76bd
Andrew Brice <bricea27@gmail.com>
2023-10-11 21:09:54 -0400

    feat: copy updates, root bg color

--- src/App.jsx
+++ src/App.jsx
@@ -11,71 +11,40 @@ export default function App() {
         <Logo className="[width:_clamp(4rem,9vw,6rem)] stroke-text stroke-[10px] mb-32 animate-draw-line fill-mode-forwards repeat-1" />
         <h1 className="font-extrabold leading-none [font-size:_clamp(2rem,7vw,5rem)] animate-fade-in-up">
           Andrew Brice
-          <br />
-          <span className="text-text-alt">
+          <span className="text-text-alt block">
             Software Engineer
             <br />& Mentor
           </span>
         </h1>
       </header>
       <section className="w-full max-w-7xl mx-auto px-8 sm:px-16 xl:px-32 py-32 animate-fade-in-up">
-        <h2 className="font-bold text-label text-xl lg:text-3xl lg:leading-[1.4] mb-4">
+        <h2 className="font-bold text-label text-xl lg:text-2xl lg:leading-[1.4] mb-4">
           About
         </h2>
-        <article className="flex flex-col lg:flex-row gap-8 lg:gap-12">
-          <p className="max-w-md lg:max-w-[50%] text-text-alt text-xl lg:text-3xl lg:leading-[1.4] flex-1">
-            Hi, I'm Andrew, a front-end engineer with ten years of experience
-            coding for the web. I leverage my background in design to craft
-            clean, user-friendly applications.
+        <article className="flex flex-wrap gap-8 lg:gap-12">
+          <p className="w-full max-w-md lg:max-w-none lg:flex-1 text-text-alt text-xl lg:text-2xl lg:leading-[1.4]">
+            Hi, I'm Andrew. I've spent the past ten years writing code for the
+            web, leveraging my background in design to craft clean,
+            user-friendly experiences. Currently, I'm building sales forecasting
+            software for the folks over at{" "}
+            <Link href="https://www.outreach.io/">Outreach</Link>, where I spend
+            my days working in React, TypeScript, and GraphQL.
           </p>
-          <p className="max-w-md lg:max-w-[50%] text-text-alt text-xl lg:text-3xl lg:leading-[1.4] flex-1">
-            I currently live in Indianapolis with my wife, son, and our two
-            dogs. As a midwest transplant, my New Jersey accent has diminished,
-            but my high pizza standards remain resolute.
+          <p className="w-full max-w-md lg:max-w-none lg:flex-1 text-text-alt text-xl lg:text-2xl lg:leading-[1.4]">
+            Originally from the east coast, I now live in Indianapolis with my
+            wife, son, and our two dogs. When I'm not writing code (or reading
+            about it), I'm either at the gym, at the movies, or at home making
+            my son laugh.
           </p>
         </article>
       </section>
-      {/* <section className="w-full max-w-7xl mx-auto px-8 sm:px-16 xl:px-32 py-32 animate-fade-in-up">
-        <h2 className="font-bold text-label text-xl lg:text-3xl lg:leading-[1.4] mb-4">Work</h2>
-        <article className="flex flex-col lg:flex-row gap-8 lg:gap-12 mb-16">
-          <p className="max-w-md lg:max-w-[50%] text-text-alt text-xl lg:text-3xl lg:leading-[1.4]">
-            Currently, I'm helping build world-class sales forecasting software
-            for the folks over at{" "}
-            <Link href="https://www.outreach.io/">Outreach</Link>.
-          </p>
-          <p className="max-w-md lg:max-w-[50%] text-text-alt text-xl lg:text-3xl lg:leading-[1.4]">
-            React.js, TypeScript, Apollo GraphQL, Material UI
-          </p>
-        </article>
-        <article className="flex flex-col lg:flex-row gap-8 lg:gap-12 mb-16">
-          <p className="max-w-md lg:max-w-[50%] text-text-alt text-xl lg:text-3xl lg:leading-[1.4]">
-            Previously, I was making meetings awesome with the team at{" "}
-            <Link href="https://www.crunchbase.com/organization/docket-fd06">
-              DocketHQ
-            </Link>{" "}
-            (sold to Zoom in 2022).
-          </p>
-          <p className="max-w-md lg:max-w-[50%] text-text-alt text-xl lg:text-3xl lg:leading-[1.4]">
-            React.js, Apollo GraphQL, Material UI, Slate.js
-          </p>
-        </article>
-        <article className="flex flex-col lg:flex-row gap-8 lg:gap-12">
-          <p className="max-w-md lg:max-w-[50%] text-text-alt text-xl lg:text-3xl lg:leading-[1.4]">
-            And prior to that, I helped improve the contact center agent
-            experience at <Link href="https://sharpencx.com/">SharpenCX</Link>.
-          </p>
-          <p className="max-w-md lg:max-w-[50%] text-text-alt text-xl lg:text-3xl lg:leading-[1.4]">
-            Bootstap, Socket.io, WebRTC, PHP
-          </p>
-        </article>
-      </section> */}
       <footer className="w-full max-w-7xl mx-auto px-8 sm:px-16 xl:px-32 py-32 animate-fade-in-up">
-        <div className="w-full flex items-start justify-between flex-col-reverse lg:flex-row gap-8 lg:gap-12">
-          <div className="flex flex-col flex-1">
-            <h2 className="font-bold text-label text-xl lg:text-3xl lg:leading-[1.4] mb-4">
+        <div className="w-full flex flex-wrap gap-8 lg:gap-12">
+          <div className=" w-full max-w-md lg:max-w-none lg:flex-1 flex flex-col">
+            <h2 className="font-bold text-label text-xl lg:text-2xl lg:leading-[1.4] mb-4">
               Let's chat
             </h2>
-            <p className="max-w-md lg:max-w-[80%] text-text-alt text-xl lg:text-3xl lg:leading-[1.4] mb-8">
+            <p className="max-w-md lg:max-w-[80%] text-text-alt text-xl lg:text-2xl lg:leading-[1.4] mb-8">
               Whether you'd like to discuss an opportunity or simply talk shop,
               I'd love to connect!
             </p>
@@ -94,7 +63,7 @@ export default function App() {
               </Link>
             </div>
           </div>
-          <div className="flex flex-1">
+          <div className="w-full max-w-md lg:max-w-none lg:flex-1 flex">
             <img className="w-1/2 max-w-xs rounded-xl" src={headshot} />
           </div>
         </div>
