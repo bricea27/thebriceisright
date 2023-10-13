45f8b61862e2adcbb69f986f2889cb4fb957a5ba
Andrew Brice <bricea27@gmail.com>
2023-10-13 15:58:17 -0400

    feat: responsive tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ export default function App() {
       <header className="w-full min-h-screen flex flex-col mx-auto p-16 justify-center items-center">
         <Logo className="max-w-[80px] lg:max-w-[120px] stroke-text stroke-[10px] mb-4 lg:mb-8 animate-draw-line fill-mode-forwards repeat-1" />
         <hgroup className="animate-fade-in-up text-center">
-          <h1 className="font-extrabold text-3xl lg:text-7xl">Andrew Brice</h1>
-          <h2 className="text-text-alt font-extrabold text-xl lg:text-5xl">
+          <h1 className="font-extrabold text-3xl lg:text-6xl lg:mb-2">Andrew Brice</h1>
+          <h2 className="text-text-alt font-extrabold text-xl lg:text-4xl">
             Software Engineer
           </h2>
         </hgroup>
@@ -20,7 +20,7 @@ export default function App() {
         <div className="w-48 h-48 rounded-xl overflow-hidden mb-8 mx-auto animate-fade-in-up ">
           <img className="w-full" src={headshot} />
         </div>
-        <p className="w-full max-w-lg text-text-alt text-lg lg:text-xl lg:leading-[1.4] mb-4 lg:mb-8 animate-fade-in-up ">
+        <p className="w-full max-w-md text-text-alt text-base lg:text-lg lg:leading-[1.4] mb-4 lg:mb-8 animate-fade-in-up ">
           Hi, I'm Andrew. I've spent the past ten years writing code for the
           web, leveraging my background in design to craft clean, user-friendly
           experiences. Currently, I'm building sales forecasting software for
@@ -28,7 +28,7 @@ export default function App() {
           <Link href="https://www.outreach.io/">Outreach</Link>, where I spend
           my days working in React, TypeScript, and GraphQL.
         </p>
-        <p className="max-w-md text-text-alt text-lg lg:text-xl lg:leading-[1.4] mb-8 animate-fade-in-up">
+        <p className="max-w-md text-text-alt text-base lg:text-lg lg:leading-[1.4] mb-8 animate-fade-in-up">
           Whether you'd like to discuss an opportunity or simply talk shop, I'd
           love to chat!
         </p>
