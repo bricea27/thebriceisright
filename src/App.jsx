6b72046c3e2c8702d0c9242a3609cc5a2438884b
Andrew Brice <bricea27@gmail.com>
2023-10-19 11:57:06 -0400

    feat: animated polka dot background

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,45 @@ import headshot from "./assets/headshot.jpg";
 
 export default function App() {
   return (
-    <main className="w-full bg-neutral-900 font-sans text-neutral-100">
-      <hgroup className="w-full flex flex-col mx-auto px-8 sm:px-16 pt-8 pb-32 sm:pt-16 justify-center max-w-7xl">
-        <Logo className="[width:_clamp(4rem,9vw,6rem)] stroke-neutral-100 stroke-[10px] mb-32 animate-draw-line fill-mode-forwards repeat-1" />
+    <main className="w-full font-sans text-slate-100">
+      <hgroup className="w-full min-h-screen flex flex-col mx-auto px-8 sm:px-16 pt-8 pb-32 sm:pt-16 justify-center max-w-7xl">
+        <Logo className="[width:_clamp(4rem,9vw,6rem)] stroke-slate-100 stroke-[10px] mb-32 animate-draw-line fill-mode-forwards repeat-1" />
         <h1 className="font-extrabold leading-none [font-size:_clamp(2rem,7vw,5rem)] animate-fade-in-up">
           Andrew Brice
-          <span className="font-normal text-neutral-600 block">
-            Software Engineer &<br />
-            Mentor
+          <span className="font-normal text-slate-500 block">
+            Software Engineer <br />& Mentor
           </span>
         </h1>
       </hgroup>
       <section className="w-full max-w-7xl mx-auto px-8 sm:px-16 py-32 animate-fade-in-up">
-        <h2 className="font-bold text-label text-xl lg:text-2xl lg:leading-[1.4] mb-4">
-          About
-        </h2>
-        <article className="flex flex-wrap gap-8 lg:gap-12">
-          <p className="w-full max-w-md lg:max-w-none lg:flex-1 text-neutral-600 text-xl lg:text-2xl lg:leading-[1.4]">
+        <article className="flex flex-wrap justify-between items-center">
+          <p className="w-full max-w-lg text-slate-500 text-xl lg:text-2xl lg:leading-[1.4]">
             Hi, I'm Andrew. I've spent the past ten years writing code for the
             web, leveraging my background in design to craft clean,
-            user-friendly experiences. Currently, I'm building sales forecasting
-            software for the folks over at{" "}
-            <Link href="https://www.outreach.io/">Outreach</Link>, where I spend
-            my days working in React, TypeScript, and GraphQL.
+            user-friendly experiences. Originally from the east coast, I now
+            live in Indianapolis with my wife, son, and our two dogs. When I'm
+            not writing code (or reading about it), I'm either at the gym, at
+            the movies, or at home making my son laugh.
           </p>
-          <p className="w-full max-w-md lg:max-w-none lg:flex-1 text-neutral-600 text-xl lg:text-2xl lg:leading-[1.4]">
-            Originally from the east coast, I now live in Indianapolis with my
-            wife, son, and our two dogs. When I'm not writing code (or reading
-            about it), I'm either at the gym, at the movies, or at home making
-            my son laugh.
+          <div className="w-full max-w-md lg:max-w-lg lg:flex-1">
+            <img className="w-1/2 max-w-xs rounded-xl shadow-lg" src={headshot} />
+          </div>
+        </article>
+      </section>
+      <section className="w-full max-w-7xl mx-auto px-8 sm:px-16 py-32 animate-fade-in-up flex">
+        <article className="flex flex-wrap justify-between items-center">
+          <h1 className="font-extrabold leading-none [font-size:_clamp(2rem,7vw,5rem)] animate-fade-in-up">
+            Make it work.
+            <br />
+            <span className="text-slate-400">Make it right.</span>
+            <br />
+            <span className="text-slate-600">Make it fast.</span>
+            <span className="text-xl font-normal text-slate-600 block">
+              - Kent Beck
+            </span>
+          </h1>
+          <p className="w-full max-w-lg text-slate-500 text-xl lg:text-2xl lg:leading-[1.4]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum atque blanditiis assumenda natus aliquid sunt aperiam labore ullam enim quisquam, voluptatem nesciunt facilis, alias ut magnam vitae distinctio? Ipsam, a!
           </p>
         </article>
       </section>
@@ -44,11 +54,11 @@ export default function App() {
             <h2 className="font-bold text-label text-xl lg:text-2xl lg:leading-[1.4] mb-4">
               Let's chat
             </h2>
-            <p className="max-w-md lg:max-w-[80%] text-neutral-600 text-xl lg:text-2xl lg:leading-[1.4] mb-8">
+            <p className="max-w-md lg:max-w-xl text-slate-600 text-xl lg:text-2xl lg:leading-[1.4] mb-8">
               Whether you'd like to discuss an opportunity or simply talk shop,
               I'd love to connect!
             </p>
-            <div className="flex flex-col gap-2 text-base lg:text-xl">
+            <div className="flex flex-col sm:flex-row gap-2 sm:gap-8 text-base lg:text-xl">
               <Link href="mailto:bricea27@gmail.com">
                 <Envelope />
                 bricea27@gmail.com
@@ -62,9 +72,6 @@ export default function App() {
                 LinkedIn
               </Link>
             </div>
-          </div>
-          <div className="w-full max-w-md lg:max-w-none lg:flex-1 flex">
-            <img className="w-1/2 max-w-xs rounded-xl" src={headshot} />
           </div>
         </div>
       </footer>
