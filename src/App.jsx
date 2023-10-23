a3366935c5d1c4274e710a9eeeb1b46aa9fff68a
Andrew Brice <bricea27@gmail.com>
2023-10-23 17:59:44 -0400

    split-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
-// import { Envelope, Github, LinkedIn } from "./components/icons";
-// import Link from "./components/Link";
+import { Envelope, Github, LinkedIn } from "./components/icons";
+import Link from "./components/Link";
 import Logo from "./components/Logo";
 
-// import headshot from "./assets/headshot.jpg";
+import headshot from "./assets/headshot.jpg";
 
 export default function App() {
   return (
-    <>
-      <header className="flex min-h-screen font-sans w-full p-8 sm:p-16 bg-gradient-to-b from-transparent via-transparent to-merino-50 items-center place-content-center">
-        <Logo className="w-[60px] stroke-merino-900 stroke-[10px] animate-draw-line fill-mode-forwards repeat-1 absolute top-8 left-8" />
+    <main className="w-full flex flex-col lg:flex-row min-h-screen">
+      <header className="flex min-h-screen font-sans w-full p-8 sm:p-16 from-transparent via-transparent to-slate-950 items-center place-content-center">
+        <Logo className="w-[60px] stroke-slate-100 stroke-[10px] animate-draw-line fill-mode-forwards repeat-1 absolute top-8 left-8" />
         <hgroup className="max-w-sm md:max-w-2xl flex flex-1 flex-col mx-auto text-center animate-fade-in-up my-32">
-          <h1 className="font-black text-merino-950 text-4xl sm:text-5xl md:text-7xl leading-none tracking-tight animate-fade-in-up mb-8 mx-auto">
+          <h1 className="font-black text-slate-100 text-4xl sm:text-5xl md:text-7xl leading-none tracking-tight animate-fade-in-up mb-8 mx-auto">
             Andrew Brice
-            <span className="block text-merino-700 text-2xl sm:text-3xl md:text-5xl">
+            <span className="block text-slate-700 text-2xl sm:text-3xl md:text-5xl">
               Software Engineer & Mentor
             </span>
           </h1>
-          <p className="font-normal text-merino-900 text-lg sm:text-xl md:text-2xl mb-4 mx-auto">
+          <p className="font-normal text-slate-100 text-lg sm:text-xl md:text-2xl mb-4 mx-auto">
             I've spent the past ten years writing code for the web, leveraging
             my background in design to craft clean, user-friendly experiences.
           </p>
         </hgroup>
       </header>
+      <section className="w-96 flex flex-col justify-center items-center p-8 sm:p-16 bg-slate-950">
+        <img
+          className=" rounded-xl shadow-lg mb-8"
+          alt="Andrew Brice"
+          src={headshot}
+        />
+        <div className="w-full max-w-7xl mx-auto lg:flex-1 flex flex-col">
+          <h2 className="font-bold text-label text-xl lg:text-2xl lg:leading-[1.4] mb-4">
+            Let's chat
+          </h2>
+          <p className="text-slate-600 text-lg lg:text-xl lg:leading-[1.4] mb-8">
+            Whether you'd like to discuss an opportunity or simply talk shop,
+            I'd love to connect!
+          </p>
+          <div className="flex gap-4 sm:gap-8 text-base lg:text-xl">
+            <Link href="mailto:bricea27@gmail.com">
+              <Envelope />
+            </Link>
+            <Link href="https://github.com/bricea27">
+              <Github />
+            </Link>
+            <Link href="https://www.linkedin.com/in/briceandrew/">
+              <LinkedIn />
+            </Link>
+          </div>
+        </div>
+      </section>
       {/* <main className="w-full bg-gradient-to-b from-transparent via-transparent to-slate-950 font-sans text-slate-100">
         <section className="w-full px-8 sm:px-16 py-64">
           <article className="max-w-7xl mx-auto flex flex-col lg:flex-row lg:items-center gap-32 lg:gap-0">
@@ -83,7 +110,7 @@ export default function App() {
         <footer className="w-full px-8 sm:px-16 py-32">
           <div className="w-full max-w-7xl mx-auto lg:flex-1 flex flex-col">
             <h2 className="font-bold text-label text-xl lg:text-3xl lg:leading-[1.4] mb-4">
-              Contact
+              Let's chat
             </h2>
             <p className="max-w-md lg:max-w-xl text-slate-600 text-xl lg:text-2xl lg:leading-[1.4] mb-8">
               Whether you'd like to discuss an opportunity or simply talk shop,
@@ -103,6 +130,6 @@ export default function App() {
           </div>
         </footer>
       </main> */}
-    </>
+    </main>
   );
 }
